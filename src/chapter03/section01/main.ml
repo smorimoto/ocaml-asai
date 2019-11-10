82002e0e6d7c528441859a0b87ce50f4ea72cf8d
BSKY <git@bsky.moe>
2019-11-11 07:34:28 +0900

    Add chapter03/section01

---
+++ src/chapter03/section01/main.ml
@@ -0,0 +1,17 @@
+(*
+(1)
+# let e = 2.7182;;
+val e : float = 2.7182
+
+(2)
+# let positive = e > 0.0;;
+val positive : bool = true
+
+(3)
+# let seconds_of_day = 60 * 60 * 24;;
+val seconds_of_day : int = 86400
+
+(4)
+# let name = "茗荷谷";;
+val name : string = "茗荷谷"
+*)
